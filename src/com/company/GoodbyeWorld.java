133a966d05840c15aaa8e3d341fea044f39fada0
John <johnisennock@gmail.com>
2020-01-18 10:01:07 -0500

    The First Commit of Programming Assignment 1

---
+++ src/com/company/GoodbyeWorld.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class GoodbyeWorld {
+        public static void goodbyeWorld(){
+            System.out.printf("Goodbye, World!");
+        }
+}
---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class HelloWorld {
+    public static void helloWorld()
+
+    {
+        System.out.printf("Hello, World!%n");
+    }
+}
